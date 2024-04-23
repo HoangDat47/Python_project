6d8fdbfbfd2bd65d32b5bea4ae444f120796e7dd
HoangDat47 <dat47707@gmail.com>
2024-04-23 13:13:02 +0700

    update

--- py_project/app2.py
+++ py_project/app2.py
@@ -1,3 +1,4 @@
+from matplotlib import pyplot as plt
 import pandas as pd
 import tkinter as tk
 from tkinter import *
@@ -20,14 +21,16 @@ from keras.layers import Dense, Conv2D, Dropout, Flatten, MaxPooling2D, Input
 from tqdm.notebook import tqdm
 
 root = tk.Tk()
-root.title('App version 1.3.6')
+root.title('App version 1.3.4')
 
-my_ref={} # to store references to checkboxes 
-i=1
-selected_checkboxes = [] # To store the checkbuttons which are checked
-data_types = ["int64", "float64", "object"]
+my_ref = {}  # to store references to checkboxes
+i = 1
+selected_checkboxes = []  # To store the checkbuttons which are checked
+labels = []
 
 # Data functions
+
+
 def upload_file():
     global df, tree_list
     f_types = [('CSV files', "*.csv"), ('All', "*.*")]
@@ -42,23 +45,16 @@ def upload_file():
     search_entry.bind('<KeyRelease>', lambda event: my_search())
     target_combobox["values"] = tree_list
     my_columns()
-    
-    # Transform tab
-    for dtype in data_types:  # Loop through data types
-        # Lọc các cột theo định dạng dữ liệu dtype
-        columns = [col for col in df.columns if df[col].dtype == dtype]
-        # Tạo danh sách tên cột cùng với định dạng dữ liệu của chúng
-        columns_with_dtype = [f"{col} {{{dtype}}}" for col in columns]
-        # Thêm danh sách cột vào Listbox
-        for col in columns_with_dtype:
-            transform_list.insert(tk.END, col)
+
 
 def my_search():
-    query = search_entry.get().strip().lower()  # Lấy giá trị từ Entry và chuyển về chữ thường
+    # Lấy giá trị từ Entry và chuyển về chữ thường
+    query = search_entry.get().strip().lower()
     if query:  # Kiểm tra xem giá trị được nhập vào hay không
         keywords = query.split()  # Tách các từ khóa
         # Tạo điều kiện tìm kiếm
-        condition = df.apply(lambda row: all(keyword.lower() in str(row).lower() for keyword in keywords), axis=1)
+        condition = df.apply(lambda row: all(keyword.lower() in str(
+            row).lower() for keyword in keywords), axis=1)
         df2 = df[condition]  # Lọc các hàng thỏa mãn điều kiện tìm kiếm
         r_set = df2.to_numpy().tolist()  # Tạo danh sách các hàng kết quả
 
@@ -67,6 +63,7 @@ def my_search():
     else:
         # Nếu không có giá trị tìm kiếm được nhập vào, hiển thị toàn bộ dữ liệu
         trv_refresh()
+
 
 def trv_refresh(r_set=None):  # Refresh the Treeview to reflect changes
     global df, trv, tree_list
@@ -93,6 +90,8 @@ def trv_refresh(r_set=None):  # Refresh the Treeview to reflect changes
     vs.grid(row=5, column=4, sticky='ns')  # Place on grid
 
 # Visualize functions
+
+
 def my_columns():
     global i, my_ref, selected_checkboxes
     i = 1  # to increase the column number
@@ -105,12 +104,15 @@ def my_columns():
         cb.grid(row=i + 3, column=0, padx=5, sticky=tk.W)
         my_ref[column] = var
         i += 1
-        selected_checkboxes.append((column, var))  # Append checkbox and its variable to the list
+        # Append checkbox and its variable to the list
+        selected_checkboxes.append((column, var))
+
 
 def execute_model():
     global model_train  # Di chuyển câu lệnh global lên đầu hàm
     target_variable = target_combobox.get()
-    input_variables = [column for column, var in selected_checkboxes if var.get() == 1]
+    input_variables = [column for column,
+                       var in selected_checkboxes if var.get() == 1]
     le = LabelEncoder()
 
     # if input variable is categorical convert to numerical
@@ -160,15 +162,17 @@ def execute_model():
         r2 = str(r2_score(y_test, y_pred))
         messagebox.showinfo("Model Result", f"Mean Squared Error: {r2}")
 
+
 def create_cnn_df(dir):
     image_paths = []
     labels = []
     for label in os.listdir(dir):
-        for imagename in os.listdir(os.path.join(dir,label)):
-            image_paths.append(os.path.join(dir,label,imagename))
+        for imagename in os.listdir(os.path.join(dir, label)):
+            image_paths.append(os.path.join(dir, label, imagename))
             labels.append(label)
         print(label, "completed")
-    return image_paths,labels
+    return image_paths, labels
+
 
 def choose_train_dir():
     global train, TRAIN_DIR
@@ -177,70 +181,116 @@ def choose_train_dir():
     train = pd.DataFrame()
     train['image'], train['label'] = create_cnn_df(TRAIN_DIR)
 
+
 def choose_test_dir():
     global test, TEST_DIR
     TEST_DIR = filedialog.askdirectory()
     test_dir_label.config(text=TEST_DIR)
     test = pd.DataFrame()
     test['image'], test['label'] = create_cnn_df(TEST_DIR)
-    
+
+
 def extract_features(images):
     features = []
     for image in tqdm(images):
-        img = load_img(image, color_mode='grayscale', target_size=(48,48))
+        img = load_img(image, color_mode='grayscale', target_size=(48, 48))
         img = np.array(img)
         features.append(img)
     features = np.array(features)
-    features = features.reshape(len(features),48,48,1)
+    features = features.reshape(len(features), 48, 48, 1)
     return features
+
 
 def train_model():
     train_features = extract_features(train['image'])
     test_features = extract_features(test['image'])
-    
+
     x_train = train_features/255.0
     x_test = test_features/255.0
-    
+
     le = LabelEncoder()
     le.fit(train['label'])
-    
+
     y_train = le.transform(train['label'])
     y_test = le.transform(test['label'])
-    
+
     y_train = to_categorical(y_train, num_classes=7)
     y_test = to_categorical(y_test, num_classes=7)
-    
+
     model = Sequential()
-    model.add(Input(shape=(48,48,1)))
+    model.add(Input(shape=(48, 48, 1)))
 
-    model.add(Conv2D(128, kernel_size=(3,3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.4))
 
-    model.add(Conv2D(256, kernel_size=(3,3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Conv2D(256, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.4))
 
-    model.add(Conv2D(512, kernel_size=(3,3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Conv2D(512, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.4))
 
-    model.add(Conv2D(512, kernel_size=(3,3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Conv2D(512, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.4))
-    
+
     model.add(Flatten())
-    
+
     model.add(Dense(128, activation='relu'))
     model.add(Dropout(0.2))
     model.add(Dense(128, activation='relu'))
     model.add(Dropout(0.2))
-    
+
     model.add(Dense(7, activation='softmax'))
-    
-    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-    model.fit(x_train, y_train, epochs=5, batch_size=64, validation_data=(x_test, y_test))
-    
+
+    model.compile(optimizer='adam',
+                  loss='categorical_crossentropy', metrics=['accuracy'])
+    model.fit(x_train, y_train, epochs=5, batch_size=64,
+              validation_data=(x_test, y_test))
+
+    model_json = model.to_json()
+    with open("trained_model.json", 'w') as json_file:
+        json_file.write(model_json)
+    model.save("trained_model.h5")
+
+
+def choose_json():
+    global json_file_path, model_json
+    json_file_path = filedialog.askopenfilename(filetypes=(("JSON files", "*.json"), ("All files", "*.*")))
+    if json_file_path:
+        json_file = open(json_file_path, 'r')
+        model_json_label.config(text=json_file_path)
+        model_json = json_file.read()
+        json_file.close()
+
+def choose_model():
+    global model, loaded_model
+    model = model_from_json(model_json)
+    model_h5_path = filedialog.askopenfilename(filetypes=(("H5 files", "*.h5"), ("All files", "*.*")))
+    if model_h5_path:
+        model_h5_label.config(text=model_h5_path)
+        model.load_weights(model_h5_path)
+        
+def choose_input():
+    global img
+    img = load_img(image, color_mode="grayscale")
+    feature = np.array(img)
+    feature = feature.reshape(1, 48, 48, 1)
+    image = filedialog.askopenfilename()
+    input_label.config(text=image)
+    img = feature/255.0
+    pred = model.predict(img)
+    pred_label = labels[pred.argmax()]
+    plt.title('Prediction: ' + pred_label)
+    plt.imshow(img.reshape(48, 48), cmap='gray')
+
+def add_label():
+    label_text = label_input.get()
+    labels.append(label_text)
+    labels_tree.insert('', 'end', value=(label_text,))
+
 # GUI
 left_frame = tk.LabelFrame(root, text='Choose File')
 left_frame.grid(row=0, column=0, padx=10, pady=10)
@@ -252,12 +302,10 @@ tabControl = ttk.Notebook(right_frame)
 tab1 = ttk.Frame(tabControl)
 tab2 = ttk.Frame(tabControl)
 tab3 = ttk.Frame(tabControl)
-tab4 = ttk.Frame(tabControl)
 
 tabControl.add(tab1, text='Data')
 tabControl.add(tab2, text='Visualize')
 tabControl.add(tab3, text='CNN model for classification')
-tabControl.add(tab4, text='Transform')
 tabControl.grid(row=0, column=0, columnspan=2)
 
 # Data tab
@@ -313,13 +361,39 @@ test_dir_btn = tk.Button(tab3, text="Browse", command=choose_test_dir)
 test_dir_btn.grid(row=1, column=1, padx=5, pady=5, sticky=tk.W)
 
 train_model_btn = tk.Button(tab3, text="Train", width=20, command=train_model)
-train_model_btn.grid(row=2, column=1, padx=5, pady=5, sticky=tk.W)
+train_model_btn.grid(row=2, column=0, padx=5, pady=5, sticky=tk.W)
 
-# Transform tab
-transform_label = tk.Label(tab4, text="Select variables(s): ")
-transform_label.grid(row=0, column=0, padx=5, pady=5, sticky=tk.W)
+or_label = tk.Label(tab3, text="Choose model from file")
+or_label = or_label.grid(row=3, column=0, padx=5, pady=5, sticky=tk.W)
 
-transform_list = tk.Listbox(tab4, height=5, selectmode=tk.SINGLE)
-transform_list.grid(row=1, column=0, padx=5, pady=5, sticky=tk.W + tk.E + tk.N + tk.S)
+model_json_label = tk.Label(tab3, text="Model json file")
+model_json_label.grid(row=4, column=0, padx=5, pady=5, sticky=tk.W)
+
+choose_json_btn = tk.Button(tab3, text="Browse", command=choose_json)
+choose_json_btn.grid(row=4, column=1, padx=5, pady=5, sticky=tk.W)
+
+model_h5_label = tk.Label(tab3, text="Model h5 file")
+model_h5_label.grid(row=5, column=0, padx=5, pady=5, sticky=tk.W)
+
+choose_model_btn = tk.Button(tab3, text="Browse", command=choose_model)
+choose_model_btn.grid(row=5, column=1, padx=5, pady=5, sticky=tk.W)
+
+labels_tree = ttk.Treeview(tab3, column="Labels", show="headings")
+labels_tree.grid(row=8, column=0, padx=5, pady=5, sticky=tk.W)
+
+add_labels_label = tk.Label(tab3, text="Add labels")
+add_labels_label.grid(row=7, column=0, padx=5, pady=5, sticky=tk.W)
+
+label_input = tk.Entry(tab3, width=20)
+label_input.grid(row=7, column=1, padx=5, pady=5, sticky=tk.W)
+
+add_label_btn = tk.Button(tab3, text="Add", command=add_label)
+add_label_btn.grid(row=7, column=2, padx=5, pady=5, sticky=tk.W)
+
+choose_input_label = tk.Label(tab3, text="Choose input picture")
+choose_input_label.grid(row=9, column=0, padx=5, pady=5, sticky=tk.W)
+
+choose_input_btn = tk.Button(tab3, text="Browse", command=choose_input)
+choose_input_btn.grid(row=9, column=1, padx=5, pady=5, sticky=tk.W)
 
 root.mainloop()  # Keep the window open
